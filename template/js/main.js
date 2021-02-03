2b641f9463eeeb83819f83537d1dca4cbc39b3ef
NDNey <ndavidney@gmail.com>
2021-02-03 11:23:46 -0500

    practicing

--- template/js/main.js
+++ template/js/main.js
@@ -1,3 +1,19 @@
-function iklk(){
-    console.log('jkbachca');
+// Make a function that does arithmetic!
+// JavaScript:
+function arithmetic(a, b, operator){
+ switch(operator){
+     case "add" :
+     return a+ b
+     break;
+     case "subtract":
+      return a- b
+     break;
+     case "multiply":
+      return a*b
+     break;
+     case "divide":
+      return a/ b
+     break;
+ }
+  
 }
