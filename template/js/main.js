d20eea5763f41f3daacb5b66aff8f3260d97cf95
NDNey <ndavidney@gmail.com>
2021-03-25 12:50:34 -0400

    js practice challenges added

--- template/js/main.js
+++ template/js/main.js
@@ -213,20 +213,3 @@ function isPangram(string){
   let answer = new Set(string.toLowerCase().replace(/[^a-z]/g, "") ).size === 26
   return answer
 }
-
-//[name:Duplicate Encoder]
-// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
-
-// Examples
-// "din"      =>  "((("
-// "recede"   =>  "()()()"
-// "Success"  =>  ")())())"
-// "(( @"     =>  "))((" 
-// Notes
-
-// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
-
-function duplicateEncode(word){
-  let answer = word.toLowerCase().split("").map((char,index,array) => array.filter(let => let === char).length > 1 ? ")" : "(" ).join("")
-  return answer
-}
